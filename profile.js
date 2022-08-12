85df25fd25aeaf4b0ceb2090d85f10f444747817
Greg <gwcorbett@gmail.com>
2022-08-12 12:57:59 -0600

    End Of Time

---
+++ profile.js
@@ -0,0 +1,20 @@
+<body>
+
+    <script src="profile.html"></script>;
+
+
+
+</body>
+
+// document.querySelector(`#color`).addEventListener(`click`, color)
+
+// function colorClick(){
+//     alert("Crimson")
+// }
+// button id="color" onclick="alert(`Crimson`)"><button;
+
+document.getElementById(`color`).addEventListener(`click`, clickColor);
+
+function clickColor(){
+    alert ("Crimson");
+}
